8cc10271a2613820dcae723e9e6ed7c59f52efeb
mhussainshah1 <muhammadshah.14@sunymaritime.edu>
2019-02-26 10:17:16 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/SpringBoot301Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot301Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot301Application.class, args);
+    }
+
+}
